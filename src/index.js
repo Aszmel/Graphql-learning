158eae08719c586fef2a0269807b0cb714ef2771
Arkadiusz Szymczak <mellipone@gmail.com>
2019-06-21 21:00:54 +0200

    basics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import { GraphQLServer } from "graphql-yoga";
+//hardcoded demo data
+const users = [
+  { id: "1", name: "arek", email: "arek@gmail.com", age: 44 },
+  { id: "2", name: "kasia", email: "kasia@gmail.com", age: 31 },
+  { id: "3", name: "lena", email: "lena@gmail.com", age: 5 }
+];
+const posts = [
+  { id: "1", title: "post 1", body: "post 1 body", published: true },
+  { id: "2", title: "post 2", body: "post 2 body", published: false },
+  { id: "3", title: "post 3", body: "post 3 body", published: true }
+];
 
 //type definitions (schema)
 const typeDefs = `
     type Query {
       me: User!
+      users(query: String): [User!]!
       post: Post!
-      greeting(name: String): String
+      posts(query: String): [Post!]!
+      
     }
 
     type User {
@@ -19,7 +32,7 @@ const typeDefs = `
       id: ID!
       title: String!
       body: String!
-      published: Boolean!
+      published: String!
     }
 `;
 
@@ -34,21 +47,41 @@ const resolvers = {
         age: 28
       };
     },
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users;
+      }
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
     post() {
       return {
         id: "123post",
         title: "post_title",
         body: "123post some text",
-        published: true
+        published: "2019-06-20"
       };
     },
-    greeting(parent, args, ctx, info) {
-      if (args.name) {
-        return `Hello ${args.name}`;
-      } else {
-        return "Hello";
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts;
       }
+      return posts.filter(post => {
+        return (
+          post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+          post.body.toLowerCase().includes(args.query.toLowerCase())
+        );
+      });
     }
+    // start of learing, *args*
+    // greeting(parent, args, ctx, info) {
+    //   if (args.name) {
+    //     return `Hello ${args.name}`;
+    //   } else {
+    //     return "Hello";
+    //   }
+    // },
   }
 };
 
